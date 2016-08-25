7161af29d058dfaba3190c7c8f4cb1028c7c4d5f
Sera Bak <serabak15@gmail.com>
2016-08-25 09:48:08 -0700

    takes map function out of success function

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,15 @@
 // define globals
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
-var $quakesList;
+
 var map;
-var template;
+
 
 $(document).on("ready", function() {
-
+	var myLatLng = {lat: 37.38, lng: -122.44};
+	map = new google.maps.Map(document.getElementById('map'), {
+							    	center: myLatLng,
+							    	zoom: 2
+								});
 
 	$.ajax({
 		method: 'GET',
@@ -22,11 +26,7 @@ $(document).on("ready", function() {
 			earthquakes: json.features
 		});
 		$('#info').append(earthquakesHtml);
-		var myLatLng = {lat: 37.38, lng: -122.44};
-		map = new google.maps.Map(document.getElementById('map'), {
-								    	center: myLatLng,
-								    	zoom: 8
-									});
+		
 		for(var i = 0; i < json.features.length; i++){
 			var markerLatLng = {
 				lat: json.features[i].geometry.coordinates[1], 
